5d52c67e9b84f77f52bd9cca1c4aba1384303404
daecazu <daecazu@gmail.com>
2016-02-22 16:44:34 -0500

    flowrouter params

--- client/recipies/Suscriptions.js
+++ client/recipies/Suscriptions.js
@@ -1,4 +1,4 @@
-
+//Recipes
 Template.Recipes.onCreated(function(){
 	var self = this;
 	self.autorun(function(){
@@ -8,6 +8,20 @@ Template.Recipes.onCreated(function(){
 Template.Recipes.helpers({
 	recipes: ()=>{
 		return Recipes.find({});
+	}
+
+});
+//Single Recipes
+Template.RecipeSingle.onCreated(function(){
+	var self = this;
+	self.autorun(function(){
+		self.subscribe('recipes')
+	});
+});
+Template.RecipeSingle.helpers({
+	recipe: ()=>{
+		var id = FlowRouter.getParam('id');
+		return Recipes.findOne({_id: id});
 	}
 
 });
