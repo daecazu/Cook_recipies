e6022695c555ec6bee99d13467ed85c5bc32d354
daecazu <daecazu@gmail.com>
2016-02-23 20:05:45 -0500

    filtering menu items

---
+++ client/Suscriptions.js
@@ -0,0 +1,41 @@
+//Recipes suscriptions
+Template.Recipes.onCreated(function(){
+	var self = this;
+	self.autorun(function(){
+		self.subscribe('recipes')
+	});
+});
+Template.Recipes.helpers({
+	recipes: ()=>{
+		return Recipes.find({});
+	}
+
+});
+//Single Recipes suscriptions
+Template.RecipeSingle.onCreated(function(){
+	var self = this;
+	self.autorun(function(){
+		var id = FlowRouter.getParam('id');
+		self.subscribe('singleRecipe',id)
+	});
+});
+Template.RecipeSingle.helpers({
+	recipe: ()=>{
+		var id = FlowRouter.getParam('id');
+		return Recipes.findOne({_id: id});
+	}
+
+});
+//Menú suscriptions
+Template.Menu.onCreated(function(){
+	var self = this;
+	self.autorun(function(){
+		self.subscribe('recipes')
+	});
+});
+Template.Menu.helpers({
+	recipes: ()=>{
+		return Recipes.find({inMenu: true});
+	}
+
+});
