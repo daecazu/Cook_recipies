39711711f9298cad2dc603b5e77a724dc8cb5eb4
daecazu <daecazu@gmail.com>
2016-02-27 17:43:14 -0500

    updating with quickform

--- client/recipies/Recipe.js
+++ client/recipies/Recipe.js
@@ -1,6 +1,14 @@
+Template.Recipe.helpers({
+	updateRecipeId: function(){
+		return this._id;
+	}
+
+});
+
 Template.Recipe.events({
  'click .toggle-menu': function(){
  	//console.log('click'); //probar que está funcionando
  	Meteor.call('toggleMenuItem',this._id, this.inMenu); 
  }
 });
+
